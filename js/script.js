05b3c08ad7b199d8d975698cc3b2d4e1251a2537
MicheleCasani <casanimichele@gmail.com>
2025-03-18 14:44:43 +0100

    [UPD] fix javascript

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 == 0) {
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log(`${i} FizzBuzz`)
+    }
+
+    else if (i % 3 == 0) {
         console.log(`${i} Fizz`)
     }
 
-    if (i % 5 == 0) {
+    else if (i % 5 == 0) {
         console.log(`${i} Buzz`)
     }
 
-    if (i % 15 == 0) {
-        console.log(`${i} FizzBuzz`)
+    else{
+        console.log(i)
     }
 }
